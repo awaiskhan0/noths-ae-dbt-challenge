af478eaea2a08d236dca77778a9e01c7c821a125
Awais Khan <awaisk@hotmail.co.uk>
2023-09-28 09:53:05 +0100

    added order_item_total, on_sale

--- models/marts/fct_order_items.sql
+++ models/marts/fct_order_items.sql
@@ -16,6 +16,10 @@ categories as (
     select * from {{ ref('stg_categories') }}
 ),
 
+snapshot_products as (
+    select * from {{ ref('snapshot_products') }}
+),
+
 final as (
     select
         order_items.id as order_item_id,
@@ -24,6 +28,12 @@ final as (
         order_items.order_id,
         order_items.product_id,
         order_items.quantity,
+        order_items.quantity * iff(
+            snapshot_products.on_sale, 
+            products.sale_price,
+            products.price
+        ) as order_item_total,
+        snapshot_products.on_sale,
         products.name as product_name,
         categories.id as category_id,
         categories.name as category_name
@@ -33,20 +43,28 @@ final as (
         on order_items.product_id = products.id
     left join categories
         on products.category_id = categories.id
+    left join snapshot_products
+        on order_items.product_id = snapshot_products.id
+    
+    where order_items.created_at >= snapshot_products.dbt_updated_at
+
+    qualify row_number() over (
+        partition by order_item_id
+        order by dbt_updated_at desc
+    ) = 1
 )
 
 select
     order_item_id,
-    order_id,
-    product_id,
-    category_id,
-
     order_item_created_at,
     order_item_updated_at,
-
-    quantity,
-
+    order_id,
+    product_id,
     product_name,
-    category_name
+    category_id,
+    category_name,
+    quantity,
+    order_item_total,
+    on_sale
 
 from final
