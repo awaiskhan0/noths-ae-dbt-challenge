d16ebc454fb0e54ec800e6c15138dadc4614c579
Awais Khan <awaisk@hotmail.co.uk>
2023-09-28 09:47:05 +0100

    removed materialised param

--- models/staging/stg_refunds.sql
+++ models/staging/stg_refunds.sql
@@ -1,19 +1,15 @@
 {{
   config (
-      materialized='table',
       unique_key='id',
       cluster_by=['created_at']
       )
 }}
 
 with source as (
-
   select * from {{ ref('refunds') }}
-
 ),
 
 renamed as (
-
   select
     id,
     created_at,
@@ -23,7 +19,6 @@ renamed as (
     refund_reason
 
   from source
-
 )
 
 select * from renamed
